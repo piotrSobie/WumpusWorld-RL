abbe4668f01b9d7f0cec69411a8e9f5f387be276
piotrSobie <48648334+piotrSobie@users.noreply.github.com>
2021-05-18 22:21:39 +0200

    Pygame upgrade save state (#6)
    * added load, save, continue learning
    
    * pygame upgrade and save upgrade

--- rl_alg/dqn/dqn_alg.py
+++ rl_alg/dqn/dqn_alg.py
@@ -7,11 +7,27 @@ import os
 
 from rl_alg.dqn.agent import Agent
 from rl_alg.dqn.utils import plot_learning
+from rl_alg.dqn.dqn_default_params import DqnDefaultParams
 
 
-def dqn_algorithm(wumpus_env, batch_size_=64, gamma_=0.999, eps_start_=1, eps_end_=0.01, eps_decay_=5e-5,
+def dqn_algorithm(wumpus_env, batch_size_=64, gamma_=0.999, eps_start_=1.0, eps_end_=0.01, eps_decay_=5e-5,
                   target_update_=100, memory_size_=100000, lr_=0.001, num_episodes_=500, max_steps_per_episode_=200,
                   save_weights_every_=50, load_state_path=None):
+
+    # default parameters
+    default_params = DqnDefaultParams()
+
+    batch_size_dqn = default_params.BATCH_SIZE
+    gamma_dqn = default_params.DISCOUNT_RATE
+    eps_start_dqn = default_params.EPSILON_START
+    eps_end_dqn = default_params.EPSILON_MIN
+    eps_decay_dqn = default_params.EPSILON_DECAY
+    target_update_dqn = default_params.TARGET_UPDATE
+    memory_size_dqn = default_params.MEMORY_SIZE
+    lr_dqn = default_params.LEARNING_RATE
+    num_episodes_dqn = default_params.NUM_EPISODES
+    max_steps_per_episode_dqn = default_params.MAX_STEPS_PER_EPISODE
+    save_weights_every_dqn = default_params.SAVE_EVERY
 
     env = wumpus_env
     if env.dqn_observation_state_number is None:
@@ -37,26 +53,75 @@ def dqn_algorithm(wumpus_env, batch_size_=64, gamma_=0.999, eps_start_=1, eps_en
     env.reset_env()
     env_action_n = env.action_space_n
     env_obs_n = env.dqn_observation_state_number
-    n_games = num_episodes_
-    max_step = max_steps_per_episode_
     start_episode = 0
-    save_weights_every = save_weights_every_
+    replay_memory_dqn = None
+    net_state_dict_dqn = None
+    optimizer_state_dict_dqn = None
 
+    # load data from file if specified
     if loaded_state is not None:
         start_episode = loaded_state['episode']
-        n_games = loaded_state['num_episodes']
-        max_step = loaded_state['max_step_per_episode']
-        save_weights_every = loaded_state['save_weights_every']
+        batch_size_dqn = loaded_state['batch_size']
+        gamma_dqn = loaded_state['gamma']
+        eps_start_dqn = loaded_state['epsilon']
+        eps_end_dqn = loaded_state['eps_min']
+        eps_decay_dqn = loaded_state['eps_dec']
+        target_update_dqn = loaded_state['replace_target']
+        lr_dqn = loaded_state['lr']
+        num_episodes_dqn = loaded_state['num_episodes']
+        max_steps_per_episode_dqn = loaded_state['max_step_per_episode']
+        save_weights_every_dqn = loaded_state['save_weights_every']
+        replay_memory_dqn = loaded_state['replay_memory']
+        net_state_dict_dqn = loaded_state['state_dict']
+        optimizer_state_dict_dqn = loaded_state['optimizer']
+
+    # overwrite settings with cmd if not none
+    if batch_size_ is not None:
+        batch_size_dqn = int(batch_size_)
+
+    if gamma_ is not None:
+        gamma_dqn = float(gamma_)
+
+    if eps_start_ is not None:
+        eps_start_dqn = float(eps_start_)
+
+    if eps_end_ is not None:
+        eps_end_dqn = float(eps_end_dqn)
+
+    if eps_decay_ is not None:
+        eps_decay_dqn = float(eps_decay_)
+
+    if target_update_ is not None:
+        target_update_dqn = int(target_update_)
+
+    if memory_size_ is not None:
+        memory_size_dqn = int(memory_size_)
+
+    if lr_ is not None:
+        lr_dqn = float(lr_)
+
+    if num_episodes_ is not None:
+        num_episodes_dqn = int(num_episodes_)
+
+    if max_steps_per_episode_ is not None:
+        max_steps_per_episode_dqn = int(max_steps_per_episode_)
+
+    if save_weights_every_ is not None:
+        save_weights_every_dqn = int(save_weights_every_)
+
+    n_games = num_episodes_dqn
+    max_step = max_steps_per_episode_dqn
+    save_weights_every = save_weights_every_dqn
 
     parameters = dict(
-        gamma=[gamma_],
-        epsilon=[eps_start_],
-        eps_dec=[eps_decay_],
-        eps_end=[eps_end_],
-        batch_size=[batch_size_],
-        lr=[lr_],
-        max_mem_size=[memory_size_],
-        replace_target=[target_update_]
+        gamma=[gamma_dqn],
+        epsilon=[eps_start_dqn],
+        eps_dec=[eps_decay_dqn],
+        eps_end=[eps_end_dqn],
+        batch_size=[batch_size_dqn],
+        lr=[lr_dqn],
+        max_mem_size=[memory_size_dqn],
+        replace_target=[target_update_dqn]
     )
 
     param_values = [v for v in parameters.values()]
@@ -68,7 +133,8 @@ def dqn_algorithm(wumpus_env, batch_size_=64, gamma_=0.999, eps_start_=1, eps_en
     for gamma, epsilon, eps_dec, eps_end, batch_size, lr, max_mem_size, replace_target in product(*param_values):
         agent = Agent(gamma=gamma, epsilon=epsilon, eps_dec=eps_dec, eps_end=eps_end, batch_size=batch_size,
                       lr=lr, max_mem_size=max_mem_size, replace_target=replace_target,
-                      n_actions=env_action_n, input_dims=env_obs_n, loaded_state=loaded_state)
+                      n_actions=env_action_n, input_dims=env_obs_n, replay_memory=replay_memory_dqn,
+                      net_state_dict=net_state_dict_dqn, optimizer_state_dict=optimizer_state_dict_dqn)
 
         scores, eps_history = [], []
         best_avg_score = None
@@ -101,6 +167,7 @@ def dqn_algorithm(wumpus_env, batch_size_=64, gamma_=0.999, eps_start_=1, eps_en
                 # total_loss = loss * batch_size
 
             scores.append(score)
+            agent.eps_strategy.update_epsilon()
             eps_history.append(agent.eps_strategy.get_epsilon())
 
             avg_score = np.mean(scores[-100:])
@@ -129,15 +196,18 @@ def dqn_algorithm(wumpus_env, batch_size_=64, gamma_=0.999, eps_start_=1, eps_en
                     'state_dict': agent.Q_eval.state_dict(),
                     'optimizer': agent.Q_eval.optimizer.state_dict(),
                     'gamma': gamma,
-                    'epsilon_strategy': agent.eps_strategy,
+                    'epsilon': agent.eps_strategy.epsilon,
+                    'eps_dec': agent.eps_strategy.eps_dec,
+                    'eps_min': agent.eps_strategy.eps_min,
                     'batch_size': batch_size,
                     'lr': lr,
                     'max_mem_size': max_mem_size,
                     'replace_target': replace_target,
                     'replay_memory': agent.memory,
-                    'num_episodes': num_episodes_,
-                    'max_step_per_episode': max_steps_per_episode_,
-                    'save_weights_every': save_weights_every
+                    'num_episodes': n_games,
+                    'max_step_per_episode': max_step,
+                    'save_weights_every': save_weights_every,
+
                 }
                 now = datetime.datetime.now()
                 date_string = f"{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}-{now.second}"
